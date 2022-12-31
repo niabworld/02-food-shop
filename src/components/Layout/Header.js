902b55ae31c39c7ca7fe618947c3466aa6b50002
niabworld <niabworld@gmail.com>
2022-12-31 10:27:24 +0900

    modify site color styles to green

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Food shop</h1>
+        <h1>Food Shop</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
